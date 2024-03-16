c180fad99f75583694a2b9d92b8c884cb2d672a9
krishna <kv510341@gmail.com>
2024-03-16 19:02:39 +0530

    intialized this project and now working on hero section

---
+++ README.md
@@ -0,0 +1,7 @@
+# Setup
+
+1. npm create vite@latest my-project -- --template react
+2. npm i
+3. install tailwind => npm install -D tailwindcss postcss autoprefixer
+4. npx tailwindcss init -p
+5. update your tailwind.config.js
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Highlights from "./components/Highlights"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+
+const App = () => {
+
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+
+    </main>
+  )
+}
+
+export default App
