93fae1818409f84bb7808103eafc9ebaec8dacb7
krishna <kv510341@gmail.com>
2024-03-23 18:56:50 +0530

    video carousel completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Highlights from "./components/Highlights"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Model from "./components/Model"
 
 const App = () => {
 
@@ -8,8 +9,8 @@ const App = () => {
     <main className="bg-black">
       <Navbar />
       <Hero />
-      {/* <Highlights /> */}
-
+      <Highlights />
+      <Model />
     </main>
   )
 }
